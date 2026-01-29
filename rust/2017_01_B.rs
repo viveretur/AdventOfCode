8d8fca07a753c9c73498a998b7113b6f1df8974e
viveretur <191765834+viveretur@users.noreply.github.com>
2026-01-29 20:44:39 +0800

    mutatio 2017_01_B.rs

--- rust/2017_01_B.rs
+++ rust/2017_01_B.rs
@@ -1,23 +1,15 @@
-use std::io;
+use std::io::{self, Read};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input = String::new();
-    let mut total = 0;
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("lineam legere non potui");
-    let line: Vec<char> = input.trim().chars().collect();
-    let len = line.len();
-    for i in 0..len {
-        if line.get(i) == line.get((i + len / 2) % len) {
-            total += line
-                .get(i)
-                .expect("Need to improve this")
-                .to_digit(10)
-                .unwrap();
-        }
-    }
-    println!("{total}");
+    io::stdin().read_to_string(&mut input)?;
+    let line = input.trim().as_bytes();
+    let half = line.len() / 2;
+    let total: usize = (0..half)
+        .filter(|&i| line[i] == line[i + half])
+        .map(|i| (line[i] - b'0') as usize)
+        .sum();
+    println!("{}", 2 * total);
     Ok(())
 }
