157c19ad8d31d290f15e2dfdd8ff6a871f0d7f98
viveretur <191765834+viveretur@users.noreply.github.com>
2026-01-31 22:19:08 +0800

    mutatio 2017_01_?.rs

--- rust/2017_01_A.rs
+++ rust/2017_01_A.rs
@@ -1,24 +1,39 @@
-use std::io;
+use std::io::{self, Read};
 
-fn main() {
-    let mut input = String::new();
-    let mut total = 0;
-    let mut last = 'X';
+fn solve(line: &str) -> i32 {
+    let bytes = line.as_bytes();
+    let mut total = 0i32;
+    let mut last = *bytes.last().expect("non vacua");
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("lineam legere non potui");
-    input = input.trim().to_string();
-    let first = input.chars().next().unwrap();
-    for i in input.chars() {
-        if i == last {
-            total += i.to_digit(10).unwrap();
+    for &b in bytes {
+        if b == last {
+            total += (b - b'0') as i32;
         }
-        last = i;
+        last = b;
     }
-    println!("{first}, {last}");
-    if first == last {
-        total += last.to_digit(10).unwrap();
-    }
+
+    total
+}
+
+fn main() -> io::Result<()> {
+    let mut line = String::new();
+    io::stdin().read_to_string(&mut line)?;
+
+    let total = solve(&line.trim());
     println!("{total}");
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn examples() {
+        use super::solve;
+
+        assert_eq!(solve("1122"), 3);
+        assert_eq!(solve("1111"), 4);
+        assert_eq!(solve("1234"), 0);
+        assert_eq!(solve("91212129"), 9);
+    }
 }
--- rust/2017_01_B.rs
+++ rust/2017_01_B.rs
@@ -1,15 +1,39 @@
 use std::io::{self, Read};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut input = String::new();
+fn solve(line: &str) -> u32 {
+    let bytes = line.as_bytes();
+    let half = bytes.len() / 2;
 
-    io::stdin().read_to_string(&mut input)?;
-    let line = input.trim().as_bytes();
-    let half = line.len() / 2;
-    let total: usize = (0..half)
-        .filter(|&i| line[i] == line[i + half])
-        .map(|i| (line[i] - b'0') as usize)
+    // trick: only used half array, rotationally symmetric, so double total.
+    let half_sum: u32 = (0..half)
+        .filter(|&i| bytes[i] == bytes[i + half])
+        .map(|i| (bytes[i] - b'0') as u32)
         .sum();
-    println!("{}", 2 * total);
+
+    half_sum * 2
+}
+
+fn main() -> io::Result<()> {
+    let mut line = String::new();
+    io::stdin().read_to_string(&mut line)?;
+
+    let total = solve(&line.trim());
+    println!("{total}");
+
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn examples() {
+        use super::solve;
+
+        assert_eq!(solve("1212"), 6);
+        assert_eq!(solve("1221"), 0);
+        assert_eq!(solve("123425"), 4);
+        assert_eq!(solve("123123"), 12);
+        assert_eq!(solve("12131415"), 4);
+    }
 }
