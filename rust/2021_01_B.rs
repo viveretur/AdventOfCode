0ffdb2aa104252b04cf1a30e5a6cf935ce6d30c1
viveretur <191765834+viveretur@users.noreply.github.com>
2026-02-01 15:21:47 +0800

    additio 2021_01_?.rs

---
+++ rust/2021_01_B.rs
@@ -0,0 +1,37 @@
+use std::io;
+
+fn solve(input: &str) -> u32 {
+    let data = input.lines().map(|s| s.parse::<u32>().unwrap());
+
+    data.clone()
+        .zip(data.skip(3))
+        .filter(|(a, d)| d > a)
+        .count() as u32
+}
+
+fn main() -> io::Result<()> {
+    let input = io::read_to_string(io::stdin())?;
+    println!("{}", solve(&input));
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn examples() {
+        use super::solve;
+
+        let data = r#"199
+200
+208
+210
+200
+207
+240
+269
+260
+263"#;
+        assert_eq!(solve(data), 5);
+    }
+}
